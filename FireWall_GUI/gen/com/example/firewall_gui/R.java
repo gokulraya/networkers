573f36f2e5b168f40dbd32d1e1b848577445166a
Veerasekhar Addepalli <vaddepal@asu.edu>
2012-12-06 00:34:00 -0700

    EWFG AS

--- FireWall_GUI/gen/com/example/firewall_gui/R.java
+++ FireWall_GUI/gen/com/example/firewall_gui/R.java
@@ -27,10 +27,9 @@ public final class R {
         public static final int menu_settings=0x7f07000a;
     }
     public static final class layout {
-        public static final int activity_ip__add=0x7f030000;
-        public static final int activity_ip__reject=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_rule_add=0x7f030003;
+        public static final int activity_ip__reject=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_rule_add=0x7f030002;
     }
     public static final class menu {
         public static final int activity_ip__add=0x7f060000;
