b1d89427102cdb72774dce1fc4439390e9ac1876
Gokul Kumar Krishnan <gkrishn6@asu.edu>
2012-12-06 01:04:40 -0700

    Basic app to add and delete rule(No database), ignore other versions.

---
+++ MainActivity/src/com/example/firewall_gui/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.firewall_gui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+//Main method begins execution here.
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	public void addRule(View view)
+	{
+		Intent intent=new Intent(this,RuleAdd.class);
+		
+		startActivity(intent);
+	}
+
+}
