dfa9ff8352c00aac5879433da0ed4458653bf54f
Kishan Maddula <emkishan@yahoo.com>
2012-12-10 14:35:59 -0700

    Latest version of server
    removed older stuff

--- androidServer/src/com/example/androidserver/MainActivity.java
+++ androidServer/src/com/example/androidserver/MainActivity.java
@@ -1,35 +1,67 @@
 package com.example.androidserver;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	TextView text;
-	String received;
+	String received="";
 	String mClientMsg = "";
 	Thread myCommsThread = null;
+	String actualPassword="";
 	protected static final int MSG_ID = 0x1337;
 	ServerSocket ss = null;
+	ArrayList<Socket> sockets = new ArrayList<Socket>();
+	ArrayList<Socket> secureSockets = new ArrayList<Socket>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView ipTv = (TextView) findViewById(R.id.ipAddress);
+		try{
+//			WifiManager wifi = (WifiManager) getSystemService(WIFI_SERVICE);
+//			WifiInfo info = wifi.getConnectionInfo();
+//			int rawLocalAddress = info.getIpAddress();
+//			byte[] rawBytes = BigInteger.valueOf(rawLocalAddress).toByteArray();
+//			InetAddress actualAddress = InetAddress.getByAddress(rawBytes);
+//			String ipAddress = actualAddress.getHostAddress().toString();
+			String ipAddress="";
+			Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+			for(;en.hasMoreElements();){
+				NetworkInterface interf = en.nextElement();
+				for(Enumeration<InetAddress> addresses=interf.getInetAddresses();addresses.hasMoreElements();){
+					InetAddress inetAddr = addresses.nextElement();
+					if(!inetAddr.isLoopbackAddress())
+						ipAddress = inetAddr.getHostAddress().toString();
+				}
+			}
+			ipTv.setText("Your IP Address is " + ipAddress);
+		}
+		catch(Exception e){
+			ipTv.setText("Unable to fetch IP Address now");
+		}
 	}
 
 	@Override
@@ -40,11 +72,114 @@ public class MainActivity extends Activity {
 	}
 
 	public void startCapture(View view){
-		//		new QuoteServer().execute();
 		TextView tv = (TextView) findViewById(R.id.textView);
 		tv.setText("Nothing from client yet");
-		this.myCommsThread = new Thread(new CommsThread());
-		this.myCommsThread.start();
+		new MakeConnectionServer().execute();
+	}
+
+	private class MakeConnectionServer extends AsyncTask<URL,Integer,Long>{
+		Resources res = getResources();
+		int success = 0;
+		protected Long doInBackground(URL...urls ){
+
+
+			try {
+				ss = new ServerSocket(Constants.NORMAL_PORT);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			int count=0;
+			while(count<Constants.THREAD_COUNT){
+				Socket s=null;
+				Message m = new Message();
+				m.what = 0x1337;
+				try {
+					if(s == null)
+						s=ss.accept();
+					sockets.add(s);
+
+					EditText password = (EditText) findViewById(R.id.password);
+					actualPassword = password.getText().toString();
+					DataInputStream in = new DataInputStream(s.getInputStream());
+					DataOutputStream out = new DataOutputStream(s.getOutputStream());
+					String sentPassword = in.readUTF();
+					String sourceIp = s.getInetAddress().toString();
+					received +=  res.getString(R.string.CONNECTION_FROM_MESSAGE) + sourceIp;
+
+					if(actualPassword.equals(sentPassword)){
+						out.writeUTF(Constants.SUCCESS);
+						received += Constants.SUCCESS_MESSAGE + s.getInetAddress() +"\n";
+						success=1;
+					}
+					else
+						out.writeUTF(Constants.FAILED);
+					myUpdateHandler.sendMessage(m);
+					sockets.add(s);
+					count++;
+					if(success==1){
+						ServerSocket secured=null;
+						try{
+							secured = new ServerSocket(Constants.SECURE_PORT);
+						}
+						catch(IOException e){
+							received += Constants.SOCKET_CONNECTION_FAILED + s.getInetAddress();
+							received += e.getMessage();
+						}
+
+						Socket securedSocket = null;
+						Message msg = new Message();
+						msg.what = 0x1337;
+						try{
+							if(securedSocket==null)
+								securedSocket = secured.accept();
+							secureSockets.add(securedSocket);
+
+							EditText passwordEt = (EditText) findViewById(R.id.password);
+							actualPassword = passwordEt.getText().toString();
+							DataInputStream secureIn = new DataInputStream(securedSocket .getInputStream());
+							DataOutputStream secureOut = new DataOutputStream(securedSocket .getOutputStream());
+							String sentMsg = secureIn.readUTF();
+							String sentSecurePassword = sentMsg.substring(0, sentMsg.indexOf(Constants.DELIMITER));
+							String website=sentMsg.substring(sentMsg.indexOf(Constants.DELIMITER)+1);
+
+							received += Constants.CONNECTED_TO_MESSAGE + website + "\n";
+
+							if(!sentSecurePassword.equals(actualPassword)){
+								throw new SecurityException();
+							}
+
+							URL url = new URL(website);
+							BufferedReader buf = new BufferedReader(new InputStreamReader(url.openStream()));
+
+							String inputLine="",tempLine;
+							while((tempLine=buf.readLine())!=null){
+								inputLine += tempLine;
+							}
+
+							secureOut.writeUTF(Constants.SENT_MESSAGE + inputLine);
+							secureSockets.add(securedSocket);
+						}
+						catch(Exception e){
+							received += "Error while writing data";
+							received += e.getMessage();
+						}
+						myUpdateHandler.sendMessage(msg);
+					}
+
+				}
+				catch(SecurityException e){
+					received += Constants.ACCESS_DENIED +s.getInetAddress();
+				}
+				catch (Exception e) {
+					received += Constants.GENERAL_EXCEPTION + s.getInetAddress();
+				}
+			}
+			return (long)count;
+		}
+		protected void onPostExecute(Long result){
+			TextView tv = (TextView) findViewById(R.id.textView);
+			tv.setText(received);
+		}
 	}
 
 	Handler myUpdateHandler = new Handler() {
@@ -60,106 +195,4 @@ public class MainActivity extends Activity {
 			super.handleMessage(msg);
 		}
 	};
-
-	class CommsThread implements Runnable {
-		public void run() {
-			Socket s = null;
-			try {
-				ss = new ServerSocket(4445);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			while (!Thread.currentThread().isInterrupted()) {
-				Message m = new Message();
-				m.what = 0x1337;
-				try {
-					if (s == null)
-						s = ss.accept();
-					BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-					String st = null;
-					st = input.readLine();
-					received += st + "IP Address : " + s.getInetAddress();
-					myUpdateHandler.sendMessage(m);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
 }
-//	private class QuoteServer extends AsyncTask<URL,Integer,Long>{
-//
-//		protected Long doInBackground(URL... urls){
-//			try{
-//				new QuoteServerThread().start();
-//			}
-//			catch(Exception e){
-//				received = received + e.getMessage();
-//			}
-//			return (long)1;
-//		}
-//
-//		protected void onPostExecute(Long result) {
-//			text = (TextView) findViewById(R.id.textView);
-//
-//			text.setText(received);
-//		}
-//	}
-//
-//	private class QuoteServerThread extends Thread {
-//
-//		protected DatagramSocket socket = null;
-//		protected BufferedReader in = null;
-//		protected boolean moreQuotes = true;
-//
-//		public QuoteServerThread() throws IOException {
-//			this("QuoteServerThread");
-//		}
-//
-//		public QuoteServerThread(String name) throws IOException {
-//			super(name);
-//			socket = new DatagramSocket(4445);
-//		}
-//
-//		public void run() {
-//			while(true){
-//				try {
-//					byte[] buf = new byte[256];
-////					ServerSocket serverSocket = new ServerSocket(4445);
-////					received += "Waiting for Connection";
-////					updateText(received);
-////					Socket socket2 = serverSocket.accept();
-////					received = received + "IP Address:" + socket2.getInetAddress();
-////					updateText(received);
-//
-//									// receive request
-//									DatagramPacket packet = new DatagramPacket(buf, buf.length);
-//									received += "Waiting for Connection";
-//									socket.receive(packet);
-//					
-//									String dString = "You are connected";
-//									updateText(dString);
-//					                buf = dString.getBytes();
-//									
-//									// send the response to the client at "address" and "port"
-//									InetAddress address = packet.getAddress();
-//									received = received + "IP Address of Current request:" + address;
-//									updateText(received);
-//									int port = packet.getPort();
-//									packet = new DatagramPacket(buf, buf.length, address, port);
-//									socket.send(packet);
-//				} 
-//				catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//				socket.close();
-//			}
-//		}
-//
-//		private void updateText(String textUpdate){
-//			text = (TextView) findViewById(R.id.textView);
-//
-//			text.setText(textUpdate);
-//		}
-//	}
-//}
